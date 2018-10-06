f3d380f8d8d2b3f6eb459f7908b0b148b1a53f1d
Unknown <thomas.simon@btopenworld.com>
2018-10-06 18:35:13 +0100

    Improved movement script

--- Thunderfury Game/Assets/Our Stuff/Scripts/PlayerMovement.cs
+++ Thunderfury Game/Assets/Our Stuff/Scripts/PlayerMovement.cs
@@ -1,17 +1,44 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using InControl;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
 public class PlayerMovement : MonoBehaviour 
 {
+	protected Rigidbody playerRb;
+
+    protected bool disableInput;
+    InputDevice inputDevice;
+
+	[SerializeField] protected float moveSpeed;
+    [SerializeField] protected float rotateSpeed;
 
 	void Start ()
 	{
-		
+		playerRb = GetComponent<Rigidbody>();
+        inputDevice = InputManager.ActiveDevice;
 	}
 	
-	void Update () 
+	void FixedUpdate()
 	{
-		
+		UpdateInputDevice(inputDevice);
+	}
+
+	void UpdateInputDevice(InputDevice inputDevice_)
+    {
+		if (disableInput)
+        	return;
+
+		float _xMove = inputDevice_.LeftStickX;
+		float _zMove = inputDevice_.LeftStickY;
+
+		Vector3 _moveHor = transform.right * _xMove;
+		Vector3 _moveVer = transform.forward * _zMove;
+
+		Vector3 _velocity = (_moveHor + _moveVer).normalized * moveSpeed;
+
+		playerRb.velocity = _velocity;
+
 	}
 }
