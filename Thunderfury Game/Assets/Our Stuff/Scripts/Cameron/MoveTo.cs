0d3208eed6cad8b2376c03380b2e5e8dde691873
Cameron <cm1crt@bolton.ac.uk>
2018-10-10 09:05:14 +0100

    AI - Move to goal script

---
+++ Thunderfury Game/Assets/Our Stuff/Scripts/Cameron/MoveTo.cs
@@ -0,0 +1,19 @@
+﻿// MoveTo.cs
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class MoveTo : MonoBehaviour
+{
+
+    public Transform goal;
+
+    void Update()
+    {
+            UnityEngine.AI.NavMeshAgent agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+
+        if (goal != null)
+            agent.destination = goal.position;
+    }
+}
+
