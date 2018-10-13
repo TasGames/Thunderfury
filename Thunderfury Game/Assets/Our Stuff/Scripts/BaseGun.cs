83913474c1c17620f93e94056ecf2307fd5b186e
Thomas Simon <thomas.simon@btopenworld.com>
2018-10-13 23:48:10 +0100

    Started target script + minor additions
    Guns now have the option to use particle effects. I can't make particles.

--- Thunderfury Game/Assets/Our Stuff/Scripts/BaseGun.cs
+++ Thunderfury Game/Assets/Our Stuff/Scripts/BaseGun.cs
@@ -15,6 +15,8 @@ public class BaseGun : MonoBehaviour
 	[SerializeField] protected bool isAutomatic;
 
 	[Title("Gun Stats")]
+	[SerializeField] protected Camera cam;
+	[SerializeField] protected ParticleSystem muzzleFlash;
 	[SerializeField] protected float fireRate;
 	[SerializeField] protected float recoil;
 	[SerializeField] protected float impact;
@@ -22,11 +24,13 @@ public class BaseGun : MonoBehaviour
 	[SerializeField] protected int maxAmmo;
 	[SerializeField] protected float range;
 	[SerializeField] [ShowIf("selectGunType", typeEnum.rayType)] [Range(0, 180)] protected float spread;
+	[SerializeField] [ShowIf("selectGunType", typeEnum.rayType)] protected GameObject hitEffect;
 
 	[Title("Bullet Stats")]
 	[SerializeField] protected float damage;
-	[SerializeField] [ShowIf("selectGunType", typeEnum.projectileType)] protected float bulletSpeed;
-	[SerializeField] [ShowIf("selectGunType", typeEnum.projectileType)] protected float bulletDespawn;
+	[SerializeField] [ShowIf("selectGunType", typeEnum.projectileType)] protected GameObject projectile;
+	[SerializeField] [ShowIf("selectGunType", typeEnum.projectileType)] protected float projectileSpeed;
+	[SerializeField] [ShowIf("selectGunType", typeEnum.projectileType)] protected float projectileTimer;
 	[SerializeField] [ShowIf("selectGunType", typeEnum.projectileType)] protected bool isExplosive;
 	
 	void Start() 
@@ -35,6 +39,12 @@ public class BaseGun : MonoBehaviour
 	}
 	
 	void FixedUpdate() 
+	{
+		if (Input.GetButtonDown("Fire1"))
+			Shoot();
+	}
+
+	void Shoot()
 	{
 		if (selectGunType == typeEnum.projectileType)
 			ProjectileType();
@@ -49,6 +59,10 @@ public class BaseGun : MonoBehaviour
 
 	void RayType()
 	{
-
+		RaycastHit hit;
+		if (Physics.Raycast(cam.transform.position, cam.transform.forward, out hit, range))
+		{
+			Debug.Log(hit.transform.name);
+		}
 	}
 }
