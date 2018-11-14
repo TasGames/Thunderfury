4f5bce3e15c17904976b798be50609a328fc591b
Cameron <cm1crt@bolton.ac.uk>
2018-11-14 18:25:56 +0000

    Wave Spawning (WIP)

--- Thunderfury Game/Assets/Our Stuff/Scripts/Cameron/EnemySpawner.cs
+++ Thunderfury Game/Assets/Our Stuff/Scripts/Cameron/EnemySpawner.cs
@@ -6,30 +6,76 @@ public class EnemySpawner : MonoBehaviour {
 
     [SerializeField] GameObject enemy;
     [SerializeField] int maxEnemies = 20;
-    [SerializeField] float spawnTime = 3.0f;
+    [SerializeField] int enemyCount;
+    [SerializeField] float spawnTime = 1;
     [SerializeField] Transform[] spawnPoints;
     static int enemies;
-    
 
     // Use this for initialization
     void Start () {
-        enemies = 0;
-        InvokeRepeating("Spawn", spawnTime, spawnTime);
+        enemyCount = 0;
+        //InvokeRepeating("Spawn", spawnTime, spawnTime);
+        StartCoroutine(SpawnWave());
 	}
-	
+
+    IEnumerator SpawnWave ()
+    {
+        yield return new WaitForSeconds(spawnTime);
+        //for (int i = 0; i < enemyCount; i++)
+        //{
+        //    Instantiate(enemy, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
+        //    enemyCount++;
+        //}
+
+        // Within a while loop
+        while (enemyCount < maxEnemies)
+        {
+            pickSpawnLocation();
+            yield return new WaitForSeconds(spawnTime);
+        }
+    }
+
 	// Update is called once per frame
 	void Update () {
 
-        if(enemies >= maxEnemies)
+        //       if(enemies >= maxEnemies)
+        //       {
+        //           CancelInvoke("Spawn");
+        //       }
+        //       else
+        //       {
+        //           int spawnPointIndex = Random.Range(0, spawnPoints.Length);
+
+        //           Instantiate(enemy, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
+        //           enemies++;
+        //       }
+	}
+
+    void pickSpawnLocation ()
+    {
+        int spawnPointIndex = Random.Range(0, spawnPoints.Length);
+
+        // Generate a random position
+        Vector3 posToSpawn = (Random.insideUnitSphere) + spawnPoints[spawnPointIndex].position;
+        posToSpawn.y = spawnPoints[spawnPointIndex].position.y;
+
+        // Check it with a Physics.OverlapSphere
+        Collider[] hitColliders = Physics.OverlapSphere(posToSpawn, 0.75f);
+
+        // If it returns any colliders (hitColliders.Length > 0)
+        if (hitColliders.Length > 0)
         {
-            CancelInvoke("Spawn");
+            // Then you cannot spawn here
+            // So generate a new position (i.e. next loop)
+            return;
         }
         else
         {
-            int spawnPointIndex = Random.Range(0, spawnPoints.Length);
-
-            Instantiate(enemy, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
-            enemies++;
+            Instantiate(enemy, posToSpawn, spawnPoints[spawnPointIndex].rotation);
+            enemyCount++;
+            return;
         }
-	}
+
+        // Spawn Lemming success, return
+    }
 }
