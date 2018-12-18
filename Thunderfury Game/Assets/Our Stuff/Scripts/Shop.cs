a2683813569b61d77b14c1855fbfe0527974ed03
Thomas Simon <thomas.simon@btopenworld.com>
2018-12-18 21:33:14 +0000

    Split display and shop into 2 classes

--- Thunderfury Game/Assets/Our Stuff/Scripts/Shop.cs
+++ Thunderfury Game/Assets/Our Stuff/Scripts/Shop.cs
@@ -6,27 +6,11 @@ using UnityEngine.UI;
 
 public class Shop : MonoBehaviour 
 {
-	[Title("Gun")]
 	[SerializeField] protected Gun gun;
 
-	[Title("Gun Details")]
-	[SerializeField] protected Text gunName;
-	[SerializeField] protected Text gunDescription;
-	[SerializeField] protected Text gunDamage;
-	[SerializeField] protected Text gunImpact;
-	[SerializeField] protected Text gunFireRate;
-	[SerializeField] protected Text gunRecoil;
-	[SerializeField] protected Text gunAmmo;
 
 	void Start() 
 	{
-		gunName.text = gun.name;
-		gunDescription.text = gun.description;
-		gunDamage.text = "Damage: " + gun.damage;
-		gunImpact.text = "Impact: " + gun.impact;
-		gunFireRate.text = "Fire Rate: " + gun.fireRate;
-		gunRecoil.text = "Recoil: " + gun.recoil;
-		gunAmmo.text = "Ammo: " + gun.magSize + " / " + gun.maxAmmo;
 
 	}
 	
