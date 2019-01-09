ef117510dada3135cd4440593308e76bb390b784
Cameron <cm1crt@bolton.ac.uk>
2019-01-09 01:54:57 +0000

    AI Improvements - Player damaging, improved hit detection

--- Thunderfury Game/Assets/Our Stuff/Scripts/Cameron/MoveTo.cs
+++ Thunderfury Game/Assets/Our Stuff/Scripts/Cameron/MoveTo.cs
@@ -9,14 +9,22 @@ public class MoveTo : MonoBehaviour
     public Transform goal;
 
     UnityEngine.AI.NavMeshAgent agent;
-
+    public PlayerHealth player;
     Animator anim;
+
+    public float damageToDeal;
+    [SerializeField] protected float damageWait;
+    protected bool dealtDamage = false;
+
+    float distance = 2.0f;
+
 
     void Start()
     {
         anim = GetComponent<Animator>();
 
         agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+
     }
 
     void Update()
@@ -26,12 +34,43 @@ public class MoveTo : MonoBehaviour
         {
             agent.destination = goal.position;
         }
+
+
+        if(Vector3.Distance(transform.position, goal.position) < distance)
+        {
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * distance, Color.red);
+
+            if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), distance))
+            {
+                if (dealtDamage == false)
+                {
+                    dealtDamage = true;
+                    player.PlayerTakeDamage(damageToDeal);
+                    Debug.Log("Dealt Damage to player");
+                    Debug.Log(player.pShield);
+                    Debug.Log(player.pHealth);
+                }
+            }
+        }
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        anim.SetTrigger("Attack");
+        agent.isStopped = true;
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-        agent.isStopped = true;
-        anim.SetTrigger("Attack");
+        if (dealtDamage == false)
+        {
+            dealtDamage = true;
+            player.PlayerTakeDamage(damageToDeal);
+            Debug.Log("Dealt Damage to player");
+            Debug.Log(player.pShield);
+            Debug.Log(player.pHealth);
+        }
     }
 
     private void OnCollisionExit(Collision collision)
@@ -40,5 +79,11 @@ public class MoveTo : MonoBehaviour
         agent.isStopped = false;
     }
 
+    IEnumerator DealtDamageRoutine()
+    {
+        yield return new WaitForSeconds(damageWait);
+
+        dealtDamage = false;
+    }
 }
 
