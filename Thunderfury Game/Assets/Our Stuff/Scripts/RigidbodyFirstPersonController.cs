f3f88789fe7c2d8bc534084121ed03b8e4919597
Thomas Simon <thomas.simon@btopenworld.com>
2019-02-04 12:50:52 +0000

    Rearranged classes for movement

--- Thunderfury Game/Assets/Our Stuff/Scripts/RigidbodyFirstPersonController.cs
+++ Thunderfury Game/Assets/Our Stuff/Scripts/RigidbodyFirstPersonController.cs
@@ -41,10 +41,113 @@ public class RigidbodyFirstPersonController : MonoBehaviour
 		public float shellOffset; //reduce the radius by that ratio to avoid getting stuck in wall (a value of 0.1f is nice)
 	}
 
+	[Serializable] public class MouseLook
+	{
+		public float xSensitivity = 2f;
+		public float ySensitivity = 2f;
+		public bool clampVerticalRotation = true;
+		public float minimumX = -90F;
+		public float maximumX = 90F;
+		public bool smooth;
+		public float smoothTime = 5f;
+		public bool lockCursor = true;
+
+		protected Quaternion characterTargetRot;
+		protected Quaternion cameraTargetRot;
+		protected bool isCursorLocked = true;
+
+		public void Init(Transform character, Transform camera)
+		{
+			characterTargetRot = character.localRotation;
+			cameraTargetRot = camera.localRotation;
+		}
+
+		public void LookRotation(Transform character, Transform camera)
+		{
+			float yRot = Input.GetAxis("Mouse X") * xSensitivity;
+			float xRot = Input.GetAxis("Mouse Y") * ySensitivity;
+
+			characterTargetRot *= Quaternion.Euler (0f, yRot, 0f);
+			cameraTargetRot *= Quaternion.Euler (-xRot, 0f, 0f);
+
+			if(clampVerticalRotation == true)
+				cameraTargetRot = ClampRotationAroundXAxis (cameraTargetRot);
+
+			if(smooth == true)
+			{
+				character.localRotation = Quaternion.Slerp (character.localRotation, characterTargetRot,
+					smoothTime * Time.deltaTime);
+				camera.localRotation = Quaternion.Slerp (camera.localRotation, cameraTargetRot,
+					smoothTime * Time.deltaTime);
+			}
+			else
+			{
+				character.localRotation = characterTargetRot;
+				camera.localRotation = cameraTargetRot;
+			}
+
+			UpdateCursorLock();
+		}
+
+		public void SetCursorLock(bool value)
+		{
+			lockCursor = value;
+			
+			if(lockCursor == false)
+			{
+				//we force unlock the cursor if the user disable the cursor locking helper
+				Cursor.lockState = CursorLockMode.None;
+				Cursor.visible = true;
+			}
+		}
+
+		public void UpdateCursorLock()
+		{
+			//if the user set "lockCursor" we check & properly lock the cursos
+			if (lockCursor)
+				InternalLockUpdate();
+		}
+
+		void InternalLockUpdate()
+		{
+			if(Input.GetKeyUp(KeyCode.Escape))
+				isCursorLocked = false;
+			else if(Input.GetMouseButtonUp(0))
+				isCursorLocked = true;
+
+			if (isCursorLocked)
+			{
+				Cursor.lockState = CursorLockMode.Locked;
+				Cursor.visible = false;
+			}
+			else if (!isCursorLocked)
+			{
+				Cursor.lockState = CursorLockMode.None;
+				Cursor.visible = true;
+			}
+		}
+
+		Quaternion ClampRotationAroundXAxis(Quaternion q)
+		{
+			q.x /= q.w;
+			q.y /= q.w;
+			q.z /= q.w;
+			q.w = 1.0f;
+
+			float angleX = 2.0f * Mathf.Rad2Deg * Mathf.Atan (q.x);
+
+			angleX = Mathf.Clamp (angleX, minimumX, maximumX);
+
+			q.x = Mathf.Tan (0.5f * Mathf.Deg2Rad * angleX);
+
+			return q;
+		}
+	}
+
 	[SerializeField] protected Camera cam;
-	public MovementSettings movementSettings = new MovementSettings();
-	public MouseLook mouseLook = new MouseLook();
-	public AdvancedSettings advancedSettings = new AdvancedSettings();
+	[SerializeField] protected MovementSettings movementSettings = new MovementSettings();
+	[SerializeField] protected MouseLook mouseLook = new MouseLook();
+	[SerializeField] protected AdvancedSettings advancedSettings = new AdvancedSettings();
 
 	protected Rigidbody m_RigidBody;
 	protected CapsuleCollider m_Capsule;
