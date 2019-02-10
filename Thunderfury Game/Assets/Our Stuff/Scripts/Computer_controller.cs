604b4508f082b91464d841e7f30c9783026f1bf5
Thomas Simon <thomas.simon@btopenworld.com>
2019-02-10 20:57:58 +0000

    Adjusted computer script + deleted events
    Animation events were pointless and causing errors
    Code is now constant with everyone else's coding style

---
+++ Thunderfury Game/Assets/Our Stuff/Scripts/Computer_controller.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Computer_controller : MonoBehaviour 
+{
+protected Animator anim;
+protected bool opened;
+
+	void Start()
+	{
+		anim = GetComponent<Animator>();
+	}
+	
+	void OnTriggerStay(Collider coll)
+	{
+		if(Input.GetKeyDown(KeyCode.E) && opened == false)
+		{
+            opened = true;
+			anim.SetBool("Open", true);
+			anim.SetBool("Close", false);
+		}
+		else if(Input.GetKeyDown(KeyCode.E) && opened == true)
+		{
+			opened = false;
+			anim.SetBool("Close", true);
+			anim.SetBool("Open", false);
+		}
+	}
+}
