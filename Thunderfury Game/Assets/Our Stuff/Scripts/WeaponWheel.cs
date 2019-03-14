bc38b0ed0256e393570efc7583f411962f68377d
Thomas Simon <thomas.simon@btopenworld.com>
2019-03-14 13:49:02 +0000

    Weapon wheel buttons highlight

--- Thunderfury Game/Assets/Our Stuff/Scripts/WeaponWheel.cs
+++ Thunderfury Game/Assets/Our Stuff/Scripts/WeaponWheel.cs
@@ -15,19 +15,34 @@ public class WeaponWheel : MonoBehaviour
 
 	[SerializeField] protected TextMeshProUGUI[] ammoText;
 	protected UseGun[] uG;
+	protected int selectedButton = 0;
+	protected Color highlightColour;
+	protected Color standardColour;
+	protected float colourValue;
+	protected float alphaValue;
 
 	[HideInInspector] public GameObject currentGun;
 
 	void Start()
 	{
-		uG[0] = rifle.GetComponent<UseGun>();
+
+	/*	uG[0] = rifle.GetComponent<UseGun>();
 		uG[1] = grenadeLauncher.GetComponent<UseGun>();
 		uG[3] = railgun.GetComponent<UseGun>();
 		uG[2] = shotgun.GetComponent<UseGun>();
 		uG[4] = singularity.GetComponent<UseGun>();
 		uG[5] = pistol.GetComponent<UseGun>();
 
-		StartCoroutine(AmmoRoutine());
+		StartCoroutine(AmmoRoutine());*/
+
+
+		highlightColour = new Color32(30, 30, 30, 190);
+		standardColour = new Color32(0, 0, 0, 190);			
+	}
+	
+	void Update()
+	{
+		ChangeButton();
 	}
 
 	public void ButtonPistol()
@@ -80,6 +95,49 @@ public class WeaponWheel : MonoBehaviour
 					ammoText[i].text = uG[i].currentMag + " / " + uG[i].ammoPool;
 			}
 			yield return new WaitForSeconds(1);
+		}
+	}
+
+	void ChangeButton()
+	{
+		int previousButton = selectedButton;
+
+		if (Input.GetAxis("Mouse ScrollWheel") > 0f)
+		{
+			if (selectedButton >= transform.childCount - 1)
+				selectedButton = 0;
+			else
+			selectedButton++;
+		}
+		else if (Input.GetAxis("Mouse ScrollWheel") < 0f)
+		{
+			if (selectedButton == 0)
+				selectedButton = transform.childCount - 1;
+			else
+			selectedButton--;
+		}
+
+		if (previousButton != selectedButton)
+			SelectButton();
+	}
+
+	void SelectButton()
+	{
+		int i = 0;
+		foreach (Transform button in transform)
+		{
+			if (i == selectedButton)
+			{
+				Image image = button.gameObject.GetComponent<Image>();
+				image.color = highlightColour;
+			}
+			else
+			{
+				Image image2 = button.gameObject.GetComponent<Image>();
+				image2.color = standardColour;
+			}
+
+			i++;
 		}
 	}
 
