6321d8c7d11f821e92e9123406f07eec68919651
Cameron <cm1crt@bolton.ac.uk>
2019-03-21 09:54:31 +0000

    Headbobbing Improved
    Removed horizontal headbobbing + changed variables in inspector

--- Thunderfury Game/Assets/Our Stuff/Scripts/HeadBobbing.cs
+++ Thunderfury Game/Assets/Our Stuff/Scripts/HeadBobbing.cs
@@ -12,12 +12,12 @@ public class HeadBobbing : MonoBehaviour
  	void Update()
     {
     	float waveslice = 0.0f;
-    	float horizontal = Input.GetAxis("Horizontal");
+    	//float horizontal = Input.GetAxis("Horizontal");
     	float vertical = Input.GetAxis("Vertical");
   
     	Vector3 cSharpConversion = transform.localPosition; 
   
-    	if (Mathf.Abs(horizontal) == 0 && Mathf.Abs(vertical) == 0)
+    	if (/*Mathf.Abs(horizontal) == 0 && */Mathf.Abs(vertical) == 0)
         	timer = 0.0f;
     	else 
 		{
@@ -31,7 +31,7 @@ public class HeadBobbing : MonoBehaviour
     	if (waveslice != 0) 
 		{
 			float translateChange = waveslice * bobbingAmount;
-			float totalAxes = Mathf.Abs(horizontal) + Mathf.Abs(vertical);
+			float totalAxes = /*Mathf.Abs(horizontal) + */Mathf.Abs(vertical);
 			totalAxes = Mathf.Clamp (totalAxes, 0.0f, 1.0f);
 			translateChange = totalAxes * translateChange;
 			cSharpConversion.y = midpoint + translateChange;
