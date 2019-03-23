16b67c7e39e84f7388efbf646da8bf44dc471224
Cameron <cm1crt@bolton.ac.uk>
2019-03-23 01:17:58 +0000

    Basic Tutorial Level Created.

---
+++ Thunderfury Game/Assets/Our Stuff/Scripts/Cameron/TutorialManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialManager : MonoBehaviour
+{
+    WaveManager wManager;
+
+    public GameObject tutShopBox;   //Tells player to open shop
+    public GameObject shopOpenBox;  //Shows text when shop opens
+    public GameObject shopScreen;   //The shop screen
+
+    bool tutShopBoxCheck;
+    bool shopOpenBoxCheck;
+
+    // Use this for initialization
+    void Start()
+    {
+        wManager = GetComponent<WaveManager>();
+        tutShopBoxCheck = true;
+        shopOpenBoxCheck = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (wManager.waveCounter >= 2 && tutShopBoxCheck)
+        {
+            tutShopBox.SetActive(true);
+            shopOpenBox.SetActive(true);
+            tutShopBoxCheck = false;
+        }
+        // if (shopScreen.activeInHierarchy && shopOpenBoxCheck)
+        // {
+        //     shopOpenBox.SetActive(true);
+        //     shopOpenBoxCheck = false;
+        // }
+    }
+}
---
+++ Thunderfury Game/Assets/Our Stuff/Scripts/Cameron/TutorialText.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+public class TutorialText : MonoBehaviour
+{
+
+    public TMP_Text text;
+
+	public float textDisplayTime;
+
+    // Use this for initialization
+    void Start()
+    {
+        //text.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            StartCoroutine(ChangeText());
+        }
+
+    }
+
+    IEnumerator ChangeText()
+    {
+		text.gameObject.SetActive(true);	//Display text
+
+        yield return new WaitForSeconds(textDisplayTime);	//Display for # secs
+		Debug.Log("Works");
+
+		text.gameObject.SetActive(false);	//Hide text
+
+		this.gameObject.SetActive(false);   //Disable trigger
+        yield break;
+    }
+
+}
