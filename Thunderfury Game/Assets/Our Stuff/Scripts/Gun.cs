015ce1345f9e6ea415fc89effc0d59a983f11c97
Thomas Simon <thomas.simon@btopenworld.com>
2019-04-03 00:53:50 +0100

    Ammo cost now in scriptable object

--- Thunderfury Game/Assets/Our Stuff/Scripts/Gun.cs
+++ Thunderfury Game/Assets/Our Stuff/Scripts/Gun.cs
@@ -39,6 +39,7 @@ public class Gun : ScriptableObject
 	
 	[Title("Ammo Stats")]
 	[EnumToggleButtons] public ammoTypeEnum takesAmmoType;
+	public int ammoCost;
 	[ShowIf("selectGunType", typeEnum.rayType)] public GameObject bullet;
 	[ShowIf("selectGunType", typeEnum.rayType)] public float damage;
 	[ShowIf("selectGunType", typeEnum.rayType)] [Range(0, 20)] public float damageRange;
