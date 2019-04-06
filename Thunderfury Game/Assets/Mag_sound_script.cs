38ed3f6fbc3dc0e89c1584ea110b97573c6fcc8c
Kade <Kadedavies@hotmail.co.uk>
2019-04-06 21:45:28 +0100

    Added Texture Updates/ Sounds
    -Added all mag sounds
    -Added Shotgun,railgun, assualt rifle, grenade launcher fire sounds
    -Added Ambient background noise, Soundmanager
    -Updated center engine textures, start ship floor texture.

---
+++ Thunderfury Game/Assets/Mag_sound_script.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mag_sound_script : MonoBehaviour {
+
+public AudioSource reload;
+
+	// Use this for initialization
+	void Start () {
+		reload.Play(0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
