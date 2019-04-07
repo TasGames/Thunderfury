3d3c9bce15277b69e71570f8b8efb930e4480cff
Thomas Simon <thomas.simon@btopenworld.com>
2019-04-07 21:29:39 +0100

    Buying ammo works!!!!!!!!!

--- Thunderfury Game/Assets/Our Stuff/Scripts/AmmoStore.cs
+++ Thunderfury Game/Assets/Our Stuff/Scripts/AmmoStore.cs
@@ -12,10 +12,13 @@ public class AmmoStore : MonoBehaviour
 		public UseGun gun;
 		public GameObject ammoHolder;
 		public TextMeshProUGUI currentAmmo;
+		public Button magButton;
+		public Button fillButton;
 		public TextMeshProUGUI magInfo;
 		public TextMeshProUGUI fillInfo;
 		public int magCost;
 		public int fillCost;
+		public int neededAmmo;
 	}
 
 	[SerializeField] protected AmmoStuff[] ammoInfo;
@@ -24,6 +27,7 @@ public class AmmoStore : MonoBehaviour
 	protected int shieldFill;
 	protected int shield10;
 	protected int fillAllCost;
+	float neededShield;
 	protected bool bootedUpOnce = false;
 
 	[SerializeField] protected TextMeshProUGUI currentShield;
@@ -43,6 +47,13 @@ public class AmmoStore : MonoBehaviour
 				ammoInfo[i].currentAmmo = ammoInfo[i].ammoHolder.GetComponent<TextMeshProUGUI>();
 				ammoInfo[i].magInfo = ammoInfo[i].ammoHolder.transform.GetChild(0).gameObject.GetComponentInChildren<TextMeshProUGUI>();
 				ammoInfo[i].fillInfo = ammoInfo[i].ammoHolder.transform.GetChild(1).gameObject.GetComponentInChildren<TextMeshProUGUI>();
+
+				ammoInfo[i].magButton = ammoInfo[i].ammoHolder.transform.GetChild(0).gameObject.GetComponent<Button>();
+				ammoInfo[i].fillButton = ammoInfo[i].ammoHolder.transform.GetChild(1).gameObject.GetComponent<Button>();
+
+				int x = i;
+				ammoInfo[i].magButton.onClick.AddListener(() => FillMag(x));
+				ammoInfo[i].fillButton.onClick.AddListener(() => FillAmmo(x));
 			}
 
 			bootedUpOnce = true;
@@ -61,8 +72,8 @@ public class AmmoStore : MonoBehaviour
 			UseGun thisGun = ammoInfo[i].gun;
 
 			ammoInfo[i].magCost = thisGun.gun.ammoCost * thisGun.prefMag;
-			int neededAmmo = thisGun.prefMaxAmmo - thisGun.ammoPool;
-			ammoInfo[i].fillCost = thisGun.gun.ammoCost * neededAmmo;
+			ammoInfo[i].neededAmmo = thisGun.prefMaxAmmo - thisGun.ammoPool;
+			ammoInfo[i].fillCost = thisGun.gun.ammoCost * ammoInfo[i].neededAmmo;
 
 			ammoInfo[i].currentAmmo.text = thisGun.gun.name + ": " + thisGun.ammoPool + " / " + thisGun.prefMaxAmmo;
 			ammoInfo[i].magInfo.text = "Mag ¥" + ammoInfo[i].magCost;
@@ -71,7 +82,7 @@ public class AmmoStore : MonoBehaviour
 			fillAllCost += ammoInfo[i].fillCost;
 		}
 
-		float neededShield = pH.maxShield - pH.pShield;
+		neededShield = pH.maxShield - pH.pShield;
 		shieldFill = shieldCost * (int)neededShield;
 
 		currentShield.text = "Shield: " + pH.pShield + " / " + pH.maxShield;
@@ -81,6 +92,26 @@ public class AmmoStore : MonoBehaviour
 		fillAllCost += shieldFill;
 
 		FullRefill.text = "Fill All	¥" + fillAllCost;
+	}
+
+	void FillAmmo(int i)
+	{
+		if (HUD.totalScore >= ammoInfo[i].fillCost)
+		{
+			ammoInfo[i].gun.ammoPool += ammoInfo[i].neededAmmo;
+			HUD.totalScore -= ammoInfo[i].fillCost;
+			UpdatePrices();
+		}
+	}
+
+	void FillMag(int i)
+	{
+		if (HUD.totalScore >= ammoInfo[i].magCost && ammoInfo[i].gun.ammoPool + ammoInfo[i].gun.prefMag <= ammoInfo[i].gun.prefMaxAmmo)
+		{
+			ammoInfo[i].gun.ammoPool += ammoInfo[i].gun.prefMag;
+			HUD.totalScore -= ammoInfo[i].magCost;
+			UpdatePrices();
+		}
 	}
 	
 }
